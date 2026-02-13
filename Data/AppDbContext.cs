b7eaf2306191180ac5157a3aaeabc8c2ebf7fa6d
Maison Keller <maisonraberkeller@gmail.com>
2026-02-13 13:31:45 -0300

    feat: adiciona Produto, AppDbContext e primeira migration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaEstoque.Api.Models;
+
+namespace SistemaEstoque.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Produto> Produtos { get; set; }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaEstoque.Api.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public decimal Preço { get; set; }
+        public int Estoque { get; set; }
+    }
+}
